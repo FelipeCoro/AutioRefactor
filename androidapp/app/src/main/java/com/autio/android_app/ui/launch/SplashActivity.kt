5b35faeabab75114d92904fe4ad8a1e9bc6a4209
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-03-06 23:46:17 -0500

    NavHostFix

--- androidapp/app/src/main/java/com/autio/android_app/ui/launch/SplashActivity.kt
+++ androidapp/app/src/main/java/com/autio/android_app/ui/launch/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.autio.android_app.R
 import com.autio.android_app.data.repository.legacy.FirebaseStoryRepository
 import com.autio.android_app.data.repository.prefs.PrefRepository
