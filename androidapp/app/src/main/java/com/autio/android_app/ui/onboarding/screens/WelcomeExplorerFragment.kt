9b8f80bf910f740bbdc741efa02bd9b7dbdf5878
Cristhian Camilo Gómez Narváez <camilogo1200@gmail.com>
2023-03-08 23:47:09 -0500

    changed bindings on fragments, Welcome Explorer fragment

--- androidapp/app/src/main/java/com/autio/android_app/ui/onboarding/screens/WelcomeExplorerFragment.kt
+++ androidapp/app/src/main/java/com/autio/android_app/ui/onboarding/screens/WelcomeExplorerFragment.kt
@@ -12,83 +12,46 @@ import com.autio.android_app.databinding.FragmentWelcomeExplorerBinding
 class WelcomeExplorerFragment :
     Fragment() {
 
-    private var _binding: FragmentWelcomeExplorerBinding? =
-        null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentWelcomeExplorerBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding =
-            FragmentWelcomeExplorerBinding.inflate(
-                inflater,
-                container,
-                false
-            )
+        binding = FragmentWelcomeExplorerBinding.inflate(
+            inflater, container, false
+        )
 
         binding.buttonLetsGo.setOnClickListener {
-            findNavController().navigate(
-                R.id.action_welcomeExplorerFragment_to_viewPagerFragment
-            )
+            findNavController().navigate(R.id.action_welcomeExplorerFragment_to_viewPagerFragment)
         }
-
         startAnimation()
-
         return binding.root
     }
 
     private fun startAnimation() {
         binding.tvWelcome.apply {
-            alpha =
-                0f
-            visibility =
-                View.VISIBLE
-            animate().alpha(
-                1f
-            )
-                .duration =
-                2000
+            alpha = 0f
+            visibility = View.VISIBLE
+            animate().alpha(1f).duration = 2000
         }
         binding.ivDivider.apply {
-            alpha =
-                0f
-            visibility =
-                View.VISIBLE
-            animate().alpha(
-                1f
-            )
-                .setDuration(
-                    2000
-                )
-                .withEndAction {
-                    binding.tvAppDescription.apply {
-                        alpha =
-                            0f
-                        visibility =
-                            View.VISIBLE
-                        animate().alpha(
-                            1f
-                        )
-                            .setDuration(
-                                2000
-                            )
-                            .withEndAction {
-                                binding.buttonLetsGo.apply {
-                                    alpha =
-                                        0f
-                                    visibility =
-                                        View.VISIBLE
-                                    animate().alpha(
-                                        1f
-                                    )
-                                        .duration =
-                                        2000
-                                }
-                            }
+            alpha = 0f
+            visibility = View.VISIBLE
+            animate().alpha(1f).setDuration(2000).withEndAction {
+                binding.tvAppDescription.apply {
+                    alpha = 0f
+                    visibility = View.VISIBLE
+                    animate().alpha(1f).setDuration(2000).withEndAction {
+                        binding.buttonLetsGo.apply {
+                            alpha = 0f
+                            visibility = View.VISIBLE
+                            animate().alpha(1f).duration = 2000
+                        }
                     }
                 }
+            }
         }
     }
 }
