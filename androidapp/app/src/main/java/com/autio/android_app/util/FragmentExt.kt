3476d8cccf4b440a5a69fd5abc55e37fdd15e902
FelipeCoro <f.coronado838@gmail.com>
2023-03-12 16:25:47 -0500

    map and functionalities merge.

--- androidapp/app/src/main/java/com/autio/android_app/util/FragmentExt.kt
+++ androidapp/app/src/main/java/com/autio/android_app/util/FragmentExt.kt
@@ -6,6 +6,12 @@ import com.autio.android_app.ui.stories.BottomNavigation
 val Fragment.bottomNavigationActivity
     get() = activity as BottomNavigation?
 
+
 fun Fragment.showFeedbackSnackBar(message: String) {
     bottomNavigationActivity?.showFeedbackSnackBar(message)
 }
+
+fun Fragment.getFloatingComponentHeight():Float?{
+   return  bottomNavigationActivity?.getFloatingComponentHeight()
+}
+
