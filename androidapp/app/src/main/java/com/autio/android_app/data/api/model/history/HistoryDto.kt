a4e0a859ef4efdf1af18492b52c7160fa55531c7
FelipeCoro <f.coronado838@gmail.com>
2023-04-07 21:49:15 -0500

    Integrated external user subscription status and receipt upload.

--- androidapp/app/src/main/java/com/autio/android_app/data/api/model/history/HistoryDto.kt
+++ androidapp/app/src/main/java/com/autio/android_app/data/api/model/history/HistoryDto.kt
@@ -1,9 +1,16 @@
 package com.autio.android_app.data.api.model.history
 
-import kotlinx.serialization.Serializable
+import com.autio.android_app.data.api.model.story.StoryDto
 
-@Serializable
+@kotlinx.serialization.Serializable
 data class HistoryDto(
-    val storyId: String,
-    val playedAt: String
+    val data: List<StoryDto>,
+    val info: Info
+)
+@kotlinx.serialization.Serializable
+data class Info(
+    val current_page: Int,
+    val records_per_page: Int,
+    val total_pages: Int,
+    val total_records: Int
 )
